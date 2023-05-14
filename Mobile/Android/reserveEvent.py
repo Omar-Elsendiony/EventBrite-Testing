d0e2609d1e602a4c27dd079e017b03b2a76c3a6e
Omar-Elsendiony <82820327+Omar-Elsendiony@users.noreply.github.com>
2023-05-14 04:52:41 +0300

    before sleep

--- Mobile/Android/reserveEvent.py
+++ Mobile/Android/reserveEvent.py
@@ -53,11 +53,8 @@ homeTab = driver.find_element(By.XPATH,"//android.widget.FrameLayout/android.vie
 
 searchTab = driver.find_element(By.XPATH,"//android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/child::android.view.View[3]/child::android.view.View[2]")
 
-likedTab = driver.find_element(By.XPATH,"//android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/child::android.view.View[3]/child::android.view.View[3]")
 
-ticketsTab = driver.find_element(By.XPATH,"//android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/child::android.view.View[3]/child::android.view.View[1]")
 
-chooseEvent = driver.find_elements(By.XPATH,"//android.view.View[@content-desc='Learn']/child::android.widget.ImageView")
 
 
 driver.implicitly_wait(3)
@@ -68,45 +65,80 @@ loginButton = driver.find_element(AppiumBy.ACCESSIBILITY_ID, value='Log In')
 loginButton.click()
 
 ###  Choosing to sign in with already signed up account not facebook nor google #######
-continueWith = driver.find_element(By.XPATH,"//android.widget.Button[@content-desc='Continue with email address']")
-continueWith.click()
-driver.implicitly_wait(3)
-enterEmail = driver.find_elements(AppiumBy.CLASS_NAME,"android.widget.EditText")
-enterEmail[0].click()
-driver.implicitly_wait(2)
-
-# ahmedsaad_2009@live.com
-# 1 - 9
-myEmail = "omar.sendiony@gmail.com"
-myPassword = "gigrgrwgg52lany"
-
-
-enterEmail[0].send_keys(myEmail)
-###  next button ##########
-driver.implicitly_wait(2)
 # input()
-enterEmail = driver.find_element(AppiumBy.XPATH,"//android.widget.Button[@content-desc='Next']")
-enterEmail.click()
 
 
-################  SignIn   #########################
-WebDriverWait(driver, 12).until(EC.presence_of_element_located((By.XPATH, "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.widget.EditText")))
+continueWith = driver.find_element(By.XPATH,"//android.widget.Button[@content-desc='Continue With Google']")
+continueWith.click()
+time.sleep(3)
+myGmail = driver.find_element(By.XPATH,"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.TextView[1]")
+myGmail.click()
+## /hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.TextView[1]
 
-password = driver.find_element(By.XPATH,"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.widget.EditText")
-password.click()
-driver.implicitly_wait(2)
-password.send_keys(myPassword)
-### Login Button ###########
-driver.implicitly_wait(3)
-loginButton = driver.find_element(By.XPATH,"//android.widget.Button[@content-desc='Log In']")
-loginButton.click()
-# driver.implicitly_wait(20)
 WebDriverWait(driver, 12).until(EC.presence_of_element_located((By.XPATH,"//android.widget.Button[@content-desc='Log out']")))
 
-
+homeTab = driver.find_element(By.XPATH,"//android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/child::android.view.View/child::android.view.View[2]/child::android.view.View[1]")
 homeTab.click()
+WebDriverWait(driver, 32).until(EC.presence_of_element_located((By.XPATH,"//android.view.View[@content-desc='Learn']")))
+time.sleep(3)
 
+chooseEvent = driver.find_elements(AppiumBy.XPATH,"//android.view.View/android.widget.ImageView")
+chooseEvent[3].click()
 
+WebDriverWait(driver, 32).until(EC.presence_of_element_located((By.XPATH,"//android.widget.ImageView")))
+
+WebDriverWait(driver, 32).until(EC.presence_of_element_located((By.XPATH,"//android.view.View[@content-desc='Organizer']")))
+
+tickets = driver.find_element(By.XPATH,"//android.widget.Button[@content-desc='Tickets']") 
+tickets.click()
+
+WebDriverWait(driver, 32).until(EC.presence_of_element_located((By.XPATH,"//android.widget.Button[@content-desc='Check out']")))
+
+plusButtons = driver.find_element(AppiumBy.XPATH,"//android.view.View/child::android.widget.Button[2]")
+plusButtons.click()
+# plusButtons[1].click()
+
+checkout = driver.find_element(By.XPATH,"//android.widget.Button[@content-desc='Check out']")
+checkout.click()
+
+WebDriverWait(driver,32).until(EC.presence_of_element_located((By.XPATH,"//android.widget.Button[@content-desc='Place order']")))
+
+# fName = driver.find_element(AppiumBy.XPATH,"//android.view.View/android.view.View/android.view.View/android.widget.ImageView/child::android.widget.EditText[3]")
+
+# fName.send_keys("omar")
+
+# secondName = driver.find_element(AppiumBy.XPATH,"//android.view.View/android.view.View/android.view.View/android.widget.ImageView/child::android.widget.EditText[4]")
+
+# secondName.send_keys("Sendo")
+
+# email = driver.find_element(AppiumBy.XPATH,"//android.view.View/android.view.View/android.view.View/android.widget.ImageView/child::android.widget.EditText[5]")
+
+# email.send_keys("omar.sendo@gmail.com")
+
+texts = driver.find_elements(By.XPATH,"//android.widget.EditText")
+texts[0].click()
+texts[0].send_keys("omar")
+driver.hide_keyboard()
+texts[1].click()
+texts[1].send_keys("send")
+driver.hide_keyboard()
+texts[2].click()
+texts[2].send_keys("mora")
+driver.hide_keyboard()
+texts[3].click()
+texts[3].send_keys("mora")
+driver.hide_keyboard()
+texts[4].click()
+texts[4].send_keys("omar.sendo@gmail.com")
+driver.hide_keyboard()
+
+driver.implicitly_wait(2)
+
+placeOrder = driver.find_element(By.XPATH,"//android.widget.Button[@content-desc='Place order']")
+placeOrder.click()
+
+# //android.view.View/child::android.widget.Button[2]
+# //android.view.View[@content-desc="Organizer"]
 
 input()
 driver.quit()
