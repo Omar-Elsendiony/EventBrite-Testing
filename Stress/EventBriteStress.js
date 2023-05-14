3c05912043ba120ada7e71ccc6d711bf9486608c
Omar-Elsendiony <82820327+Omar-Elsendiony@users.noreply.github.com>
2023-05-14 03:03:45 +0300

    last last

--- Stress/EventBriteStress.js
+++ Stress/EventBriteStress.js
@@ -6,7 +6,7 @@ export let options = {
     insecureSkipTLSVerify: true,
     noConnectionReuse: false,
     thresholds: {
-        http_req_duration: ['p(95)<1800'] // el 1750 dyh milli seconds
+        http_req_duration: ['p(95)<2000'] // el 1750 dyh milli seconds
                                          // checks if the fastest 90% have finished their requests in less than 1050 ms
     },
     stages: [
@@ -29,7 +29,7 @@ export let options = {
         { duration: '20s', target: 200 },  // keeps users at that number for 20 seconds
 
         // // Recovery stage
-        // { duration: '3m', target: 0 },  // returns to 0 users.
+        { duration: '2m', target: 0 },  // returns to 0 users.
 
     ],
     // vus :1,
@@ -38,23 +38,29 @@ export let options = {
 
 const API_BASE_URL = 'http://44.210.36.161:5000';
 const requestHeaders = {     
-        'Authorization': 'Bearer '+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdGFydCI6MTY4NDAwMTI2NS4wMTg2OTIsImV4cCI6MTY4NDA4NzY2NSwiaWQiOiI2NDVhNWQyMDM0NDA4ZTczM2JhNTY5OTYiLCJlbWFpbCI6ImFobWVkc2FhZF8yMDA5QGxpdmUuY29tIn0.c7L2VvRnHS_cyxrtHo5_CJ2N8-S33Iz7FL-8tqkinz8', // ahmed saad token :))
+        'Authorization': 'Bearer '+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdGFydCI6MTY4NDAyMjMxOS41NDQ5MjcsImV4cCI6MTY4NDEwODcxOSwiaWQiOiI2NDVhNWQyMDM0NDA4ZTczM2JhNTY5OTYiLCJlbWFpbCI6ImFobWVkc2FhZF8yMDA5QGxpdmUuY29tIn0.Qvfms7vkVSVxlRlMDr0HBL9B-hO3Rq7jc4Hz2noXTsI', // ahmed saad token :))
 };
 // var data = '';
 
 
 export default () => {
+
+    const responses = []
+    let params = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+    let payload = {
+        username: "ahmedsaad_2009@live.com",
+        password: "123456789" ,
+    };
+    let resp = http.post(API_BASE_URL+'/auth/login', payload, params);
+    responses.push(resp)
+    // console.log(resp.body)
+
     let catReq = { method: 'GET', url: API_BASE_URL+'/categories/', params: { headers: requestHeaders } }
     let res = http.get(API_BASE_URL+'/categories/')
 
     check(res, {
         'is status 200': (r) => r.status === 200
     });
-    // console.log(res.status)
-    // let data = {
-        
-    // }
-    // let createCat = { method: 'POST', url: API_BASE_URL+'/categories/', params: { headers: requestHeaders,body: } }
 
     let categoryName = "Sports & Fitness"
     let getCategoryByName = { method: 'GET', url: API_BASE_URL+'/categories/'+categoryName, 
@@ -73,23 +79,77 @@ export default () => {
     let getAvatar = { method: 'GET', url: API_BASE_URL+'/users/email/info/avatar?email='+email, 
         params: { headers: requestHeaders } 
     }
+
+    let userInfo = { method: 'GET', url: API_BASE_URL+'/users/me/info', 
+    params: { headers: requestHeaders } 
+    }
+    // let idUser = userInfo
+
+    let liked = { method: 'GET', url: API_BASE_URL+'/users/me/event/liked', 
+    params: { headers: requestHeaders } 
+    }
+
+    let followingInfo = { method: 'GET', url: API_BASE_URL+'/users/me/user/following', 
+    params: { headers: requestHeaders } 
+    }
+
+    let orders = { method: 'GET', url: API_BASE_URL+'/orders/myorders/', 
+    params: { headers: requestHeaders } 
+    }
+
+    let isAuthorized = { method: 'PUT', url: API_BASE_URL+'/auth/verify-email/', 
+    params: { headers: requestHeaders } 
+    }
+
+    // payload = {
+    //     new_password : "123456789"
+    // }
+    // // let changePass = { method: 'PUT', url: API_BASE_URL+'/auth/change-password', 
+    // // params: { headers: requestHeaders } , JS
+    // // }
+
+    // let result = http.put(API_BASE_URL+'/auth/change-password', payload, params);
+
+
+    let eventsSearch = { method: 'GET', url: API_BASE_URL+'/events/search?city=Cairo', 
+    params: { headers: requestHeaders } 
+    }
+
     const requests = http.batch([
-        catReq,getCategoryByName,getCategorySubCategories,checkEmail,getAvatar
+        catReq,getCategoryByName,getCategorySubCategories,checkEmail,getAvatar,userInfo,
+        liked,followingInfo,orders,isAuthorized,eventsSearch
     ]);
     
+    let idUser = null
     for (let i = 0 ; i < requests.length;i++){
         check(requests[i], {
             'is status 200': (r) => r.status === 200
         });
+        if (i == 5){
+            // console.log(requests[i].body)
+            res = JSON.parse(requests[i].body)
+            idUser = res.id
+            // console.log(idUser)
+        }
     }
+    if (idUser != null){
+        console.log(idUser)
+    }
+
+    // const requests_2 = http.batch([
+    //     catReq,getCategoryByName,getCategorySubCategories,checkEmail,getAvatar,userInfo,
+    //     liked,followingInfo
+    // ]);
+
+
     // let body = urlencode({ username: "ahmedsaad_2009@live.com", password: "123456789" });
-    const responses = []
-    let params = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
-    let payload = {
+    
+    params = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+    payload = {
         username: "ahmedsaad_2009@live.com",
         password: "123456789" ,
     };
-    let resp = http.post(API_BASE_URL+'/auth/login', payload, params);
+    resp = http.post(API_BASE_URL+'/auth/login', payload, params);
     responses.push(resp)
     // console.log(resp.body);
     payload = {
@@ -120,15 +180,6 @@ export default () => {
     // check email
 
 
-    // const req3 = {
-    //     method: 'POST',
-    //     url: 'http://mysirius.me:3000/home/retweet',
-    //     params: {
-    //       headers: { 'Authorization': 'Bearer ' +'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyOGQ5NzUwNGUyNjA1NzJjZGEzYWFlMyIsImlhdCI6MTY1MzQ0NjU0MywiZXhwIjoxNjYyMDg2NTQzfQ.c4KeGfQmkpir4L-nmPJpMgANjmhFhtXuD7LRSyoG6MU'},
-    //       body: { tweetID: '6267e4828e27c30923a68588',},
-    //     },
-    //   };
-    // const responses = http.batch([req3]);
 
     sleep(2)
 };
